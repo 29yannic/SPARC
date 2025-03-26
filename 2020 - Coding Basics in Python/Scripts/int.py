0156f2f0c3bcd3adc5cf8c916bd32d5c68020d83
Yannic <123062916+yleemann@users.noreply.github.com>
2025-03-26 09:01:04 +0100

    update main

---
+++ 2020 - Coding Basics in Python/Scripts/int.py
@@ -0,0 +1,36 @@
+# Assignment
+my_solution = 42 # int
+my_other_solution = 34.0 # float
+
+# Use of operators
+my_solution = my_solution - 10
+print(my_solution) # > 32
+
+my_other_solution *= 1 / 3
+print(my_other_solution) # > 11.33...
+
+print(16 + 4) # > 20
+print(7 / 2) # > 3.5
+
+print(4.0 > 4) # > False
+print(4.0 >= 4) # > True
+print(4.1 > 4) # > True
+
+print(16 % 4) # > 0
+print(16 % 7) # > 2
+
+print(2 ** 3) # > 8
+
+# Use of built-in functions
+print(int(3.2)) # > 3
+print(int('4')) # > 4
+
+print(float(3)) # > 3.0
+print(float('3.8')) # > 3.8
+
+print(abs(-4.2)) # > 4.2
+
+print(round(3.1415, 3)) # > 3.142
+
+# Oops
+print(10 / False) # Program crashes :( -> ZeroDivisionError
